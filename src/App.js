5dfdaf25a8edc49a6f48b51f4cdded391db47d8e
Emil Gamidov <dayde1991@mail.ru>
2024-08-15 17:50:41 +0500

    Added style and barebones of two components: Customer and BalancyDisplay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import Customer from "./Customer";
+import BalanceDisplay from "./BalanceDisplay";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <Customer />
+      <BalanceDisplay />
+    </div>
+  );
 }
 
 export default App;
