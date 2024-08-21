32870cb57f637cac91c5b4c3578f7666336b490d
Emil Gamidov <dayde1991@mail.ru>
2024-08-22 00:23:38 +0500

    Re-write customerSlice component to Redux Toolkit with built-in action creators

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -21,10 +21,9 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    // dispatch(deposit(depositAmount, currency));
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
-    // setCurrency("USD");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
